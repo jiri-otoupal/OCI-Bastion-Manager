4df8aa04f9f38864b905f72ce5828a6bf25e0024
Jiri Otoupal <jiri-otoupal@ips-database.eu>
2026-03-18 16:06:20 +0100

    upgrade oci version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     python_requires=">=3.7,<4",
-    install_requires=["click", "rich", "inquirerpy", "oci==2.150.0", "lastversion",
+    install_requires=["click", "rich", "inquirerpy", "oci", "lastversion",
                       "semantic_version", "pyperclip",
                       "bext", "deepmerge~=2.0"],
     extras_require={
